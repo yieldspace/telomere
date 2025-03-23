0967089b3776d9f3782cf5d8b554d2b879a35b07
tignear <tignear+m@gmail.com>
2025-03-23 22:48:57 +0900

    cargo fix

--- tests/wast.rs
+++ tests/wast.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use telomere::{common::Instance, instantiate, Module, ResultValue, WasmValue};
-use tracing::{error, level_filters::LevelFilter, Level};
+use tracing::error;
 use wast::{
     core::{NanPattern, WastRetCore},
     parser::ParseBuffer,
