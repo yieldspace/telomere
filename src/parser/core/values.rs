e869199b5a4c4198efe97b39700a85fbbc9e443a
tignear <tignear+m@gmail.com>
2025-03-27 14:23:00 +0900

    fmt

--- src/parser/core/values.rs
+++ src/parser/core/values.rs
@@ -6,8 +6,7 @@ use tracing::trace;
 pub type Result<R> = std::result::Result<R, WasmParserError>;
 
 pub fn parse_u32<R: BinaryReader>(reader: &mut R) -> Result<(usize, u32)> {
-    Leb128Parser::new(reader)
-        .parse_u32(std::mem::size_of::<u32>() * 8)
+    Leb128Parser::new(reader).parse_u32(std::mem::size_of::<u32>() * 8)
 }
 
 pub fn parse_i32<R: BinaryReader>(reader: &mut R) -> Result<(usize, i32)> {
