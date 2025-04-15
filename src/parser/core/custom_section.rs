6e942db92fdb6d3327178d3bcd18ea2368eabd50
tignear <tignear+m@gmail.com>
2025-04-15 22:00:43 +0900

    chore: fmt

--- src/parser/core/custom_section.rs
+++ src/parser/core/custom_section.rs
@@ -1,5 +1,7 @@
 use crate::binary::BinaryReader;
-use crate::common::custom_section::{FuncNameSubSec, LocalNameSubSec, ModuleNameSubSec, NameSubSection};
+use crate::common::custom_section::{
+    FuncNameSubSec, LocalNameSubSec, ModuleNameSubSec, NameSubSection,
+};
 
 use super::base::WasmBaseParser;
 use super::{Result, WasmParserError};
