d330570ce9db16f048eba6d6cf9189fc8cfd8165
tig <tignear+m@gmail.com>
2025-05-20 13:32:53 +0900

    feat(type): component import

--- crates/telomere/src/parser/component_model/import.rs
+++ crates/telomere/src/parser/component_model/import.rs
@@ -10,14 +10,20 @@ pub fn parse_import(ctx: &mut ParseContext<impl BinaryReader>) -> ParseResult<()
     let name = parse_import_name_dash(ctx)?;
     let desc = parse_externdesc(ctx)?;
     match desc {
-        ExternDesc::Component(_) => {
-            // todo register type
-            ctx.state
-                .scope_mut()
-                .add_import(&name, ComponentImport::Component);
-            ctx.state
+        ExternDesc::Component(type_id) => {
+            let global_idx = ctx
+                .state
                 .component_store
                 .register(Relation::Import(PlaceholderId::new(&name)));
+            let focus = ctx.state.scope_mut();
+            focus.add_import(&name, ComponentImport::Component);
+            focus.components.register(global_idx);
+
+            let focus = ctx.validator.scope_mut();
+            focus.component_indexes.add(type_id);
+            focus
+                .imports
+                .insert(name.original, Generic::new(GenericBound::Eq(type_id)));
         }
         ExternDesc::Instance(_) => {
             // todo register type
--- crates/telomere/tests/component_model_testsuite/instance.wast
+++ crates/telomere/tests/component_model_testsuite/instance.wast
@@ -4,7 +4,7 @@
     (instance (instantiate 0))
 )
 
-;; inline function import
+;; function import
 (component
     (type (func (result u32)))
     (import "b" (func (type 0)))
@@ -13,4 +13,24 @@
         (import "a" (func (type 0)))
     )
     (instance (instantiate 0 (with "a" (func 0))))
+)
+;; component import
+(component
+    (type 
+        (component
+            (type (func (result u32)))
+            (import "x" (func (type 0)))
+        )
+    )
+    (import "b" (component (type 0)))
+    (component
+        (type 
+            (component
+                (type (func (result u32)))
+                (import "x" (func (type 0)))
+            )
+        )
+        (import "a" (component (type 0)))
+    )
+    (instance (instantiate 1 (with "a" (component 0))))
 )
